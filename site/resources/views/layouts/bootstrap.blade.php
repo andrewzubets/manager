fca81c8f9bf6b922ac30be5ce156f9a587ca2d54
Andrew Zubets <andrewzubets@gmail.com>
2024-07-07 14:18:17 +0300

    Added static bootstrap css.

---
+++ site/resources/views/layouts/bootstrap.blade.php
@@ -0,0 +1,14 @@
+<html data-bs-theme="dark">
+<head>
+    <title>@yield('title') | Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <script src="/js/jquery-3.7.1.min.js" ></script>
+    <script src="/js/bootstrap.min.js" ></script>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
+
+
--- site/routes/web.php
+++ site/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bootstrap-test', function () {
+    return view('bootstrap-test');
+})->name('bootstrap-test');
